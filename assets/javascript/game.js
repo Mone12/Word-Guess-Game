f29aac908b31005b0773f3cbd17eaad68fc23063
Mone Duncans-Francis <mone.eunique12@gmail.com>
2019-10-22 16:00:18 -0700

    updating html

---
+++ assets/javascript/game.js
@@ -0,0 +1,34 @@
+// Constellation Theme
+
+// Create an array of outerspace object
+
+var constellation = [
+  "aries",
+  "cancer",
+  "gemini",
+  "hercules",
+  "ursa major",
+  "ursa minor",
+  "orion",
+  "pegasus",
+  "taurus",
+  "sagittarius",
+  "leo",
+  "pisces",
+  "virgo",
+  "libra",
+  "ophiuchus",
+  "perseus",
+  "capricorn",
+  "hydra",
+  "scorpio",
+  "triangulum",
+  "centaurus"
+];
+
+// variables that hold wins/losses/guessmade/guessesleft/rightguess
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var guessesMade = [];
+var guessesRight = [];
