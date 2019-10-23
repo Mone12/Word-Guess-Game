74347fa4c4259bc24dd0ae393d11010f1ade7f11
Mone Duncans-Francis <mone.eunique12@gmail.com>
2019-10-23 12:58:54 -0700

    updated javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -29,6 +29,14 @@ var constellation = [
 // variables that hold wins/losses/guessmade/guessesleft/rightguess
 var wins = 0;
 var losses = 0;
-var guessesLeft = 10;
-var guessesMade = [];
 var guessesRight = [];
+var guessesLeft = 10;
+var guessesWrong = [];
+
+// Elements hold ID references
+var winsText = document.getElementById("wins-text");
+var lossesText = document.getElementById("losses-text");
+var guessesRtext = document.getElementById("guessesright-text");
+var guessesLtext = document.getElementById("guessesleft-text");
+var guessesWtext = document.getElementById("guesseswrong-text");
+
