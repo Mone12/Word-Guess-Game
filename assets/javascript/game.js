5ae4e79a192fb5c02d777dc5c9dfcb788a118851
Mone Duncans-Francis <mone.eunique12@gmail.com>
2019-10-23 22:22:01 -0700

    updated javascript file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 // Constellation Theme
 
-// Create an array of outerspace object
+// Create an array of outerspace object and letters
 
-var constellation = [
+var constellations = [
   "aries",
   "cancer",
   "gemini",
@@ -25,18 +25,72 @@ var constellation = [
   "triangulum",
   "centaurus"
 ];
+var userOptions = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+];
 
 // variables that hold wins/losses/guessmade/guessesleft/rightguess
-var wins = 0;
-var losses = 0;
-var guessesRight = [];
-var guessesLeft = 10;
-var guessesWrong = [];
+
+ var wins = 0;
+ var rightLetters = [];
+ var guessesLeft = 10;
+ var guessedLetters = [];
+ var hiddenWord =[];
+ var randomChoices = 
+ constellations[Math.floor(Math.random() * constellations.length)];
 
 // Elements hold ID references
 var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
 var guessesRtext = document.getElementById("guessesright-text");
 var guessesLtext = document.getElementById("guessesleft-text");
 var guessesWtext = document.getElementById("guesseswrong-text");
 
+// Computer randomly chooses word
+
+    for (var index = 0; index < randomChoices.length; index++) {
+        hiddenWord.push("_");
+}
+    guessesRtext.textContent = hiddenWord.join(" ");
+
+    for (var index = 0; index < computersChoice.length; index++) {
+       computersChoice.push(computersChoice[index]);
+}
+
+  // User input
+  document.onkeyup = function(event) {
+
+    userGuess = event.key;
+    
+    guessedLetters.push(userGuess);
+
+}
+  console.log(showBlanks);
+
+// Display user input
+// Display wins and losses
+// Restart function
